837b21d3a0bcbb0516511d36492f35890c23168f
Anushka23Yadav <140548424+Anushka23Yadav@users.noreply.github.com>
2023-08-22 01:20:37 +0530

    Delete protfolio project directory

--- protfolio project/js/script.js
+++
@@ -1,8 +0,0 @@
-//  typing animation
-var typing=new Typed(".typing", {
-    strings: ["", "Frontend Developer", "Web Developer"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    backdelay:100,
-    loop: true,
-});
