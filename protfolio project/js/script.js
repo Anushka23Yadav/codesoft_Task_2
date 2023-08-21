a44596008130ae42d54724e0c81a266cb336b40e
Anushka23Yadav <140548424+Anushka23Yadav@users.noreply.github.com>
2023-08-22 00:45:02 +0530

    Add files via upload

---
+++ protfolio project/js/script.js
@@ -0,0 +1,8 @@
+//  typing animation
+var typing=new Typed(".typing", {
+    strings: ["", "Frontend Developer", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    backdelay:100,
+    loop: true,
+});
